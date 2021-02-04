931d1372b6cf0380aa6d8fe73ee4c5aab11002b7
Stas <stas.chernomorets@icloud.com>
2021-02-04 18:15:57 +0800

    first commit

---
+++ desktop/src/com/mygdx/game/desktop/DesktopLauncher.java
@@ -0,0 +1,15 @@
+package com.mygdx.game.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.mygdx.game.FirtstGame;
+
+import org.lwjgl.Sys;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new FirtstGame(), config);
+		System.out.println("hello");
+	}
+}
